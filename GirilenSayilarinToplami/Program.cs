f83a6be448c910aee471ac19a957b87a9cec42b4
Umut Söngüt <1umut.songut@gmail.com>
2022-12-15 21:32:35 +0300

    Add project files.

---
+++ GirilenSayilarinToplami/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GirilenSayilarinToplami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int toplam = 0;
+            int deneme = 0;
+
+            while (x > 0)
+            {
+                Console.Write("Bir Sayı Giriniz----> ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                toplam += x;
+                deneme++;
+
+                Console.WriteLine($"{deneme}.Denemede Girilen Sayıların Toplamı---> {toplam}\n");
+            }
+
+            Console.Read();
+        }
+    }
+}
